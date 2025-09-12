386f703310432b88b8cd51620c46edc9e84bc0ad
Shubham68201 <ramp68201@gmail.com>
2025-09-12 18:48:19 +0530

    todo list first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import AddTodo from "./components/AddTodo/AddTodo"
+import TodoList from "./components/TodoList/TodoList"
+import TodoContext from "./components/context/TodoContext";
+
+
+function App() {
+  const [list, setList] = useState([
+        {id: 1, todoData:'todo 1', finished: false},
+        {id: 2, todoData:'todo 2', finished: true},
+    ]);
+
+  return (
+    <TodoContext.Provider value={{list, setList}}>
+    <AddTodo updateList={(todo)=> setList([...list, {id:list.length+1, todoData: todo, finished: false}])}/>
+    <TodoList list={list} updateList={setList}/>
+    </TodoContext.Provider>
+  )
+}
+
+export default App
